4e0e000e85a515202feb76585c6203ecb142f20b
Marcelo Velasquez <marceloo.velasquez@hotmail.com>
2023-02-16 17:20:05 -0500

    Antd added

---
+++ src/components/App/index.jsx
@@ -0,0 +1,5 @@
+import { Events } from '../Events'
+
+export function App() {
+  return <Events></Events>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
+import { App as Antd, ConfigProvider } from 'antd'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { App } from './components/App/index'
+import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider componentSize="small">
+      <Antd>
+        <App />
+      </Antd>
+    </ConfigProvider>
   </React.StrictMode>,
 )
